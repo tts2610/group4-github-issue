642a7f8911c962549ffaf5e200e1ec145e7d190e
Sean <psytst@nottingham.ac.uk>
2020-06-04 15:25:35 +0700

    change app.js to function

---
+++ .history/src/App_20200604152404.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import "./App.css";
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const axios = require("axios");
+
+export default function App () {
+  // componentDidMount() {
+  //   axios
+  //     .get("https://api.github.com/repos/facebook/react/issues")
+  //     .then(function (response) {
+  //       console.log(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     })
+  //     .finally(function () {
+  //       // always executed
+  //     });
+  // }
+  
+    
+    const existingToken = sessionStorage.getItem("token");
+    const accessToken =
+      window.location.search.split("=")[0] === "?access_token"
+        ? window.location.search.split("=")[1].split("&")[0]
+        : null;
+
+    if (!accessToken && !existingToken) {
+      window.location.replace(
+        `https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`
+      );
+    }
+
+    if (accessToken) {
+      console.log(`New accessToken: ${accessToken}`);
+
+      sessionStorage.setItem("token", accessToken);
+      this.state = {
+        token: accessToken,
+      };
+    }
+
+    if (existingToken) {
+      this.state = {
+        token: existingToken,
+      };
+    }
+  }
+  
+    return <div></div>;
+}
