9e27f3b40ee316bbe7bfaa007dde2eef05fd2d98
Sean <psytst@nottingham.ac.uk>
2020-06-05 10:37:09 +0700

    merging

--- .history/src/App_20200605102301.js
+++ .history/src/App_20200605102301.js
@@ -52,7 +52,7 @@ function App() {
     let url = `https://api.github.com/repos/${issues}/issues`;
     let data = await fetch(url);
     let result = await data.json();
-    // console.log("what is result", result);
+    console.log("what is result", result);
     if (result.message === "Not Found") {
       setWarningMessage(
         "Your search yields no result. Please enter a valid owner/repos"
@@ -85,7 +85,6 @@ function App() {
 
   useEffect(() => {
     getToken();
-    getIssues("facebook/react");
   }, []);
 
   return (
@@ -100,7 +99,7 @@ function App() {
         onHide={handleClose}
       />
       <AlyssaModal postNewIssues={postNewIssues} />
-      <IssuesTable result={result} />
+      {/* <IssuesTable result={result} /> */}
     </div>
   );
 }
---
+++ .history/src/components/IssuesTable_20200605102823.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import Issue from "./Issue";
+
+export default class IssuesTable extends Component {
+  state = {
+    issueList: [],
+  };
+  componentDidMount() {
+    console.log(this.props.result);
+    // this.fetchIssues();
+    this.setState({ issueList: this.props.result });
+  }
+
+  // fetchIssues() {
+  //   axios
+  //     .get(`https://api.github.com/repos/facebook/react/issues?per_page=50`)
+  //     .then((res) => {
+  //       const issues = res.data;
+  //       console.log(issues);
+  //       this.setState({
+  //         issueList: issues,
+  //       });
+  //     });
+  // }
+  render() {
+    if (this.state.issueList != null)
+      return (
+        <div className="issue-table">
+          <Container>
+            <ul className="list-group mb-3">
+              <li className="list-group-item bg-light">
+                <Row>
+                  <Col sm={6}>
+                    <i className="fal fa-exclamation-circle mr-3"></i>
+                    <span>502 Open</span>
+                    <span className="text-muted ml-4">8,716 Closed</span>
+                  </Col>
+                  <Col sm={6}>
+                    <div className="filter text-muted">
+                      <span className="">Author</span>
+                      <span className="">Label</span>
+                      <span className="">Projects</span>
+                      <span className="">Milestones</span>
+                      <span className="">Assignee</span>
+                      <span>Sort</span>
+                    </div>
+                  </Col>
+                </Row>
+              </li>
+              {this.state.issueList.map(function (issue, index) {
+                return <Issue key={index} issue={issue} />;
+              })}
+            </ul>
+          </Container>
+        </div>
+      );
+    else {
+      return <div></div>;
+    }
+  }
+}
